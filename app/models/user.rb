8e048fc2398efd7e7c4880cf136e27b5f97eb0cf
seji1122 <68591263+seji1122@users.noreply.github.com>
2020-08-14 14:41:23 +0900

    バリデーション、ユーザー機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    validates :password, length: {minimum: 6}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  end
   
 end
